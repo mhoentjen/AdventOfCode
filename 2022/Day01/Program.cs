06b2f5a26d3ebec821d1726e728f2c91c30beacd
Michiel Hoentjen <michiel.hoentjen@baseflow.com>
2022-12-01 13:31:05 +0100

    Completed day 1

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = ParseInput(File.ReadLines(@"../../../input.txt").ToList());
+
+Console.WriteLine($"Part 1, final answer is {Part1(input)}");
+Console.WriteLine($"Part 2, final answer is {Part2(input)}");
+
+
+List<Elf> ParseInput(IReadOnlyList<string> inputToParse)
+{
+    var elves = new List<Elf> { new() };
+    foreach (var line in inputToParse)
+    {
+        if (string.IsNullOrEmpty(line))
+        {
+            elves.Add(new Elf());
+            continue;
+        }
+        elves.Last().Food.Add(int.Parse(line));
+    }
+    return elves;
+}
+
+int Part1(IEnumerable<Elf> elves)
+{
+    var orderedElves = elves.OrderByDescending(elf => elf.Food.Sum());
+    return orderedElves.First().Food.Sum();
+}
+
+int Part2(IEnumerable<Elf> elves)
+{
+    var orderedElves = elves.OrderByDescending(elf => elf.Food.Sum());
+    return orderedElves.Take(3).Sum(elf => elf.Food.Sum());
+}
+
+internal record Elf
+{
+    internal List<int> Food { get; } = new();
+}
